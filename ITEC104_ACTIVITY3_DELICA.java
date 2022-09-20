53b3584650090e57e91c5552890ccad5ce21de7b
Gabriel Delica <113229092+Gabbcutie@users.noreply.github.com>
2022-09-20 15:05:18 +0900

    Add files via upload

---
+++ ITEC104_ACTIVITY3_DELICA.java
@@ -0,0 +1,18 @@
+package javafundamentals;
+
+public class Arrays {
+  
+public static void main(String[] args) {
+    
+    String emails[] = {"gdelica1220@gmail.com", "gabodelica20@gmail.com", "gabdelica@gmail.com", "gdelica20@gmail.com"};
+    String usernames[] = {"Gab", "Gabo", "Gabriel", "Gabby"};
+    String passwords[] = {"12345678", "02212623", "12252000", "24681012"};
+ 
+    int i = 0;
+    System.out.printIn("Index: " + 0);
+    System.out.printIn("Email: " + emails[0]);
+    System.out.printIn("Username: " + usernames[0]);
+    System.out.printIn("Password: " + passwords[0]);
+
+} 
+} 
